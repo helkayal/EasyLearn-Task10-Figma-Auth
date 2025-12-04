b4afec7fe2da12b7d5f542b4e1b1bd5a72d8c2ef
Hoda Elkayal <helkayal169@gmail.com>
2025-12-04 22:09:47 +0200

    acd text theme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,5 +4,33 @@ import 'package:task10_figma_auth/core/theme/app_colors.dart';
 abstract final class AppTheme {
   static ThemeData appTheme = ThemeData(
     scaffoldBackgroundColor: AppColors.whiteColor,
+
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        color: AppColors.blueColor,
+        fontSize: 42,
+        fontWeight: FontWeight.w700,
+      ),
+      bodyLarge: TextStyle(
+        color: AppColors.greyColor,
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyMedium: TextStyle(
+        color: AppColors.greyColor,
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineLarge: TextStyle(
+        color: AppColors.whiteColor,
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineMedium: TextStyle(
+        color: AppColors.yellowColor,
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
   );
 }
