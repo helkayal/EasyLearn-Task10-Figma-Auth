11962718f469e2d5887a5ea41932ab7e38c0a51a
Hoda Elkayal <helkayal169@gmail.com>
2025-12-04 22:00:15 +0200

    custom buttom file

---
+++ lib/widgets/custom_nutton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:task10_figma_auth/core/theme/app_colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  final Color bgColor;
+  final Color textColor;
+
+  const CustomButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.bgColor = AppColors.blueColor,
+    this.textColor = AppColors.whiteColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: bgColor,
+        foregroundColor: textColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+          side: BorderSide(color: AppColors.blueColor, width: 2),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      ),
+      child: Text(label),
+    );
+  }
+}
