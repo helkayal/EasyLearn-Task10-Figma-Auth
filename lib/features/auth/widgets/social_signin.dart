660445f7f0ed2cad1727e17644da7a36f3e7fcd2
Hoda Elkayal <helkayal169@gmail.com>
2025-12-05 23:50:21 +0200

    update signin

--- lib/features/auth/widgets/social_signin.dart
+++ lib/features/auth/widgets/social_signin.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task10_figma_auth/core/theme/app_colors.dart';
 
-class SocialSignin extends StatelessWidget {
+class SocialSignin extends StatefulWidget {
   final String label;
   final String icon;
   final VoidCallback onPressed;
@@ -14,12 +14,29 @@ class SocialSignin extends StatelessWidget {
   });
 
   @override
+  State<SocialSignin> createState() => _SocialSigninState();
+}
+
+class _SocialSigninState extends State<SocialSignin> {
+  bool isPressed = false;
+
+  @override
   Widget build(BuildContext context) {
+    Color bgColor = isPressed
+        ? AppColors.blueColor.withValues(alpha: .2)
+        : Colors.transparent;
+
     return InkWell(
-      onTap: onPressed,
+      onTapDown: (_) => setState(() => isPressed = true),
+      onTapUp: (_) {
+        setState(() => isPressed = false);
+        widget.onPressed();
+      },
+      onTap: widget.onPressed,
       child: Container(
         height: 50,
         decoration: BoxDecoration(
+          color: bgColor,
           border: Border.all(color: AppColors.blueColor, width: 1),
           borderRadius: BorderRadius.circular(8),
         ),
@@ -27,8 +44,8 @@ class SocialSignin extends StatelessWidget {
           spacing: 10,
           children: [
             SizedBox(width: MediaQuery.of(context).size.width * 0.20),
-            Image.asset(icon, height: 20, width: 20, fit: BoxFit.cover),
-            Text(label, style: Theme.of(context).textTheme.titleMedium),
+            Image.asset(widget.icon, height: 20, width: 20, fit: BoxFit.cover),
+            Text(widget.label, style: Theme.of(context).textTheme.titleMedium),
           ],
         ),
       ),
